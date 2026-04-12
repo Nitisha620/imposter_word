8dd2c50c3f049d97c8417cf1198c96e94900d97d
Nitisha <nitishajain94@gmail.com>
2026-04-12 17:29:28 +0530

    Added models

---
+++ lib/models/eliminated_entry.dart
@@ -0,0 +1,4 @@
+class EliminatedEntry {
+  final String id, name;
+  const EliminatedEntry({required this.id, required this.name});
+}
---
+++ lib/models/pill_item.dart
@@ -0,0 +1,5 @@
+class PillItem {
+  final String value;
+  final String label;
+  const PillItem(this.value, this.label);
+}
